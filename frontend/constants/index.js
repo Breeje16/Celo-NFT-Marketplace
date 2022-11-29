92784ed301a4a26c6fb55c48bb856c2ffd45fe61
Breeje16 <breeje2001@gmail.com>
2022-11-29 19:03:59 +0530

    Project Ready!

---
+++ frontend/constants/index.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL ="https://thegraph.com/hosted-service/subgraph/breeje16/celo-nft-marketplace"
+
+export const MARKETPLACE_ADDRESS = "0xCf6304862C4f3E5B32132575F9090B777EBd349C";
